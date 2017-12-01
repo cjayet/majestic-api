436f81d9f5c8e71218d3fde8bded04c64c0e5c03
cjayet <c.jayet@optimiz.me>
2017-12-01 09:32:32 +0100

    first commit

---
+++ src/MajesticAPIService.php
@@ -0,0 +1,58 @@
+<?php namespace cjayet\Majestic;
+
+use GuzzleHttp\Client;
+
+class MajesticAPIService {
+
+    private $endpoint = "http://api.majestic.com/api/";
+    
+    public function __construct($apiKey, $sandbox = false)
+    {
+        if($sandbox == true) {
+            //$this->endpoint = "http://developer.majestic.com/api";
+            $this->endpoint = "http://developer.majestic.com/api";
+        }
+        $this->responseType = "json";
+        $this->apiKey = $apiKey;
+    }
+
+    public function setResponseType($type)
+    {
+        $this->responseType = $type;
+    }
+
+    public function executeCommand($command, $params = array())
+    {
+		$client = new GuzzleHttp\Client();
+		
+		$params["cmd"]         = $command;
+        $params["app_api_key"] = $this->apiKey;
+		
+		$res = $client->request('GET', $this->endpoint ."/". $this->responseType, [
+			'query' => $params
+		]);
+		
+    }
+
+    public function __call($name, $arguments)
+    {
+        $command = ucfirst($name);
+        if(isset($arguments[1])) {
+            $params  = $arguments[1];
+        } else {
+            $params = array();
+        }
+
+        if(is_string($arguments[0])) {
+            $params['item'] = $arguments[0];
+        } elseif(is_array($arguments[0])) {
+            $counter = 0;
+            foreach ($arguments[0] as $url) {
+                $params['item' . $counter] = $url;
+                $counter++;
+            }
+            $params['items'] = $counter;
+        }
+        return $this->executeCommand($command, $params);
+    }
+}
